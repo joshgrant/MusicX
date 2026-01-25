4ab0b7d4bc5daae12f991548c7982e62b2901e5e
Joshua Grant <joshgrant@pm.me>
2026-01-24 22:37:47 -0600

    Disable macro validation
    Signed-off-by: Joshua Grant <joshgrant@pm.me>

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,3 +9,6 @@ mise install # Installs the version from .mise.toml
 # Runs the version of Tuist indicated in the .mise.toml file {#runs-the-version-of-tuist-indicated-in-the-misetoml-file}
 mise exec -- tuist install --path ../ # `--path` needed as this is run from within the `ci_scripts` directory
 mise exec -- tuist generate -p ../ --no-open # `-p` needed as this is run from within the `ci_scripts` directory
+
+# Disable macro validation
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
